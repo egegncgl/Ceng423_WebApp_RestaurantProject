97c923cdcbe2b63b70567a051db3965a1153d5a0
Zeynep Afat <zynpaft19@gmail.com>
2023-06-15 02:44:46 +0300

    Restaurant ve Loign-signup viewleri yazıldı routeları eklendi

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace Ceng423_WebApp_RestaurantProject.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Restaurants()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
