79cfcee547cd5bffeb48c29908be17875e465ad1
egegncgl <egegncgl@gmail.com>
2023-06-17 21:12:35 +0300

    RestaurantEndpoint&MenuEndpoint

---
+++ Services/IResponseFormatter.cs
@@ -0,0 +1,7 @@
+﻿namespace Ceng423_WebApp_RestaurantProject.Services
+{
+    public interface IResponseFormatter
+    {
+        Task Format(HttpContext context, string data);
+    }
+}
---
+++ Services/TypeBroker.cs
@@ -0,0 +1,8 @@
+﻿namespace Ceng423_WebApp_RestaurantProject.Services
+{
+    public class TypeBroker
+    {
+        private static IResponseFormatter formatter = new TextResponseFormatter();
+        public static IResponseFormatter Formatter => formatter;
+    }
+}
